07a18d90dcd7a482090d531b519c57d614390453
Ngoc Nam Nguyen <nguyenngocnam242@gmail.com>
2018-11-09 14:33:17 +0700

    update code by Nam

---
+++ app/src/main/java/com/example/nam/travel/views/location/detailLocation/LoveLoctionActivity.java
@@ -0,0 +1,13 @@
+package com.example.nam.travel.views.location.detailLocation;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class LoveLoctionActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_love_loction);
+    }
+}
