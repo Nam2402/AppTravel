decc30e014a25001714fb3d21453f2a0aa8a9eb8
Ngoc Nam Nguyen <nguyenngocnam242@gmail.com>
2018-11-10 16:05:12 +0700

    update code by Nam

--- app/src/main/java/com/example/nam/travel/views/place/PlaceActivity.java
+++ app/src/main/java/com/example/nam/travel/views/place/PlaceActivity.java
@@ -2,12 +2,15 @@ package com.example.nam.travel.views.place;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.support.design.widget.CollapsingToolbarLayout;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
+import android.support.v7.widget.Toolbar;
 import android.view.MenuItem;
 import android.widget.ImageView;
 import android.widget.Toast;
+
 import com.example.nam.travel.R;
 import com.example.nam.travel.api.ApiClient;
 import com.example.nam.travel.api.ApiImageClient;
@@ -29,22 +32,17 @@ public class PlaceActivity extends AppCompatActivity implements IPlaceActivity {
     private PlacePresenter placePresenter;
     private PlaceActivity seft = this;
     private Long idPlaceType;
+    private Toolbar toolbar;
+    private CollapsingToolbarLayout collapsingToolbarLayout;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_place);
         // actionBar
-        if(getSupportActionBar() != null){
-            getSupportActionBar().setDisplayHomeAsUpEnabled(true);
-            getSupportActionBar().setDisplayShowCustomEnabled(true);
-        }
+
         Intent intent = getIntent();
         idPlaceType = intent.getLongExtra("idType", 3L);
         this.mapped();
-    }
-
-    public boolean onOptionsItemSelected(MenuItem item){
-        return super.onOptionsItemSelected(item);
     }
 
 
@@ -52,8 +50,32 @@ public class PlaceActivity extends AppCompatActivity implements IPlaceActivity {
         this.imageView = (ImageView) findViewById(R.id.toppicture);
         this.recyclerView = (RecyclerView) findViewById(R.id.my_recycler_view);
         recyclerView.setLayoutManager(new LinearLayoutManager(this));
+        initCollapsingToolbar();
         placePresenter = new PlacePresenter(this);
         placePresenter.getPlaceCategory(idPlaceType);
+    }
+
+    private void initCollapsingToolbar() {
+        toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+        setTitle("Địa Điểm");
+        //Display back home button
+        getSupportActionBar().setHomeButtonEnabled(true);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        collapsingToolbarLayout = findViewById(R.id.collapsing_toolbar);
+        collapsingToolbarLayout.setExpandedTitleColor(getResources().getColor(android.R.color.transparent));
+    }
+
+
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case android.R.id.home:
+                onBackPressed();
+                return true;
+        }
+        return super.onOptionsItemSelected(item);
     }
 
 
