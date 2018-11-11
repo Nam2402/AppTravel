c81b109b9255c53f650f44b33b683879d9f362fb
Ngoc Nam Nguyen <nguyenngocnam242@gmail.com>
2018-11-11 22:14:39 +0700

    update code

--- app/src/main/java/com/example/nam/travel/views/location/allLocation/AllLocationActivity.java
+++ app/src/main/java/com/example/nam/travel/views/location/allLocation/AllLocationActivity.java
@@ -1,9 +1,12 @@
 package com.example.nam.travel.views.location.allLocation;
 
+import android.support.design.widget.CollapsingToolbarLayout;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
+import android.support.v7.widget.Toolbar;
+import android.view.MenuItem;
 
 import com.example.nam.travel.R;
 import com.example.nam.travel.models.locationOfPlaceCategory.BaseLocation;
@@ -18,6 +21,8 @@ public class AllLocationActivity extends AppCompatActivity implements IBaseLocat
     private RecyclerView recyclerView;
     private LocationMoreAdapter locationMoreAdapter;
     private List<BaseLocation> baseLocationList;
+    private Toolbar toolbar;
+    private CollapsingToolbarLayout collapsingToolbarLayout;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -27,11 +32,35 @@ public class AllLocationActivity extends AppCompatActivity implements IBaseLocat
         recyclerView.setLayoutManager(new LinearLayoutManager(this));
         newLocationPresenter= new NewLocationPresenter(this);
         getDataLocation();
+        initCollapsingToolbar();
+
 
     }
     private void getDataLocation() {
         //get data place
         newLocationPresenter.getNewLocation();
+    }
+    private void initCollapsingToolbar() {
+        toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+//        setTitle(nameType);
+        //Display back home button
+        getSupportActionBar().setHomeButtonEnabled(true);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        collapsingToolbarLayout = findViewById(R.id.collapsing_toolbar);
+        collapsingToolbarLayout.setExpandedTitleColor(getResources().getColor(android.R.color.transparent));
+    }
+
+
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case android.R.id.home:
+                onBackPressed();
+                return true;
+        }
+        return super.onOptionsItemSelected(item);
     }
 
     @Override
