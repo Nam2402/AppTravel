20e98e87341d9d154e2c1e52a2c91e14fb20aa05
Ngoc Nam Nguyen <nguyenngocnam242@gmail.com>
2018-12-18 19:32:36 +0700

    update code

--- app/src/main/java/com/example/nam/travel/constant/Constant.java
+++ app/src/main/java/com/example/nam/travel/constant/Constant.java
@@ -2,8 +2,8 @@ package com.example.nam.travel.constant;
 
 public class Constant {
     public static final Long ID_TYPE_PLACE = 3L;
-    public static final Long ID_TYPE_EAT_DRINK = 4L;
-    public static final Long ID_TYPE_REST = 5L;
+    public static final Long ID_TYPE_EAT_DRINK = 5L;
+    public static final Long ID_TYPE_REST = 4L;
     public static final Long ID_TYPE_SHOPPING = 6L;
 
     public static final String NAME_TYPE_PLACE = "Địa Điểm";
